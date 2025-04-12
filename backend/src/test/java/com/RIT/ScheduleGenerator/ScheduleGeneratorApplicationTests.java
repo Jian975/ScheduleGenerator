1efbda2fc94dec056840c5218d2771b1978c60c6
Xuejian Sundvall <jiansundvall@gmail.com>
2025-04-12 11:19:09 -0400

    first commit

---
+++ backend/src/test/java/com/RIT/ScheduleGenerator/ScheduleGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RIT.ScheduleGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
