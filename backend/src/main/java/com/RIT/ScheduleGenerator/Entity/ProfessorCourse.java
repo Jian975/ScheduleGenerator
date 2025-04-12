cafc59a712e7728d1b1b02f1dadbef7c0a1291ee
Ryan Tagen <ryantagen@ryans-mbp.wireless.rit.edu>
2025-04-12 12:02:17 -0400

    proessor course

---
+++ backend/src/main/java/com/RIT/ScheduleGenerator/Entity/ProfessorCourse.java
@@ -0,0 +1,9 @@
+package com.RIT.ScheduleGenerator.Entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class ProfessorCourse {
+    private Long courseID;
+    private Long professorID;
+}
