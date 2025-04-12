968fd63737cff9bac46ec2f537df166682420335
Ryan Tagen <ryantagen@ryans-mbp.wireless.rit.edu>
2025-04-12 11:58:07 -0400

    person object

--- backend/src/main/java/com/RIT/ScheduleGenerator/Entity/Person.java
+++ backend/src/main/java/com/RIT/ScheduleGenerator/Entity/Person.java
@@ -11,8 +11,19 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private int age;
+
+    public Long getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 
 }
