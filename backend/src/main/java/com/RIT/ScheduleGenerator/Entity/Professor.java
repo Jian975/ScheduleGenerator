9fcfc347bd00c7e286928090216cb9d31cbcd97a
Ryan Tagen <ryantagen@ryans-mbp.wireless.rit.edu>
2025-04-12 11:53:55 -0400

    professor object

---
+++ backend/src/main/java/com/RIT/ScheduleGenerator/Entity/Professor.java
@@ -0,0 +1,23 @@
+package com.RIT.ScheduleGenerator.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String name;
+    private Double rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+}
