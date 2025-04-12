2c98cd0284d3b2704d0c874cb7b3481ea0d8d04e
Xuejian Sundvall <jiansundvall@gmail.com>
2025-04-12 11:52:22 -0400

    add DTO example

---
+++ backend/src/main/java/com/RIT/ScheduleGenerator/DTO/MessageDTO.java
@@ -0,0 +1,17 @@
+package com.RIT.ScheduleGenerator.DTO;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+
+@Builder(setterPrefix = "with")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record MessageDTO(
+    String success,
+    String failure
+) {}
+
+
